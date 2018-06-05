7b33e3f24f811348e914a293fef6a57afb5d107b
周越 <37110930+fuhe12123@users.noreply.github.com>
2018-06-05 15:39:06 +0800

    Update loginservlet.java

--- loginservlet.java
+++ loginservlet.java
@@ -1,26 +1,50 @@
- import java.io.IOException;
- import javax.servlet.ServletException;
- import javax.servlet.http.HttpServlet;
- import javax.servlet.http.HttpServletRequest;
- import javax.servlet.http.HttpServletResponse;
- import javax.servlet.http.HttpSession;
- 
- public class LoginServlet extends HttpServlet {
- 	@Override
- 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
- 			throws ServletException, IOException {
- 		// TODO Auto-generated method stub
- 		doPost(req, resp);
- 	}
- 
- 	@Override
- 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
- 			throws ServletException, IOException {
- 		String user = req.getParameter("user");
- 		HttpSession session = req.getSession();
- 		session.setAttribute("user", user);
- 		resp.sendRedirect("index.jsp");
- 
- 	}
- 
- }
+package com.wzc.loginServlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wzc.loginDao.UserDao;
+
+public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String verifyc  = request.getParameter("verifycode");
+		String svc =(String) request.getSession().getAttribute("sessionverify");
+		String psw =new UserDao().findUser(username);
+		if(!svc.equalsIgnoreCase(verifyc)){
+			request.setAttribute("username", username);
+			request.setAttribute("msg1", "* 验证码错误");
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+			return;
+		}
+		if(psw ==null){
+			request.setAttribute("username", username);
+			request.setAttribute("msg2", "* 用户不存在");
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+			return;
+		}
+		if(psw!=null&&!psw.equals(password)){
+			request.setAttribute("username", username);
+			request.setAttribute("msg3", "* 密码错误");
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+			return;
+		}
+		if(psw.equals(password)){
+			request.setAttribute("msg", "用户："+username+",登陆成功");
+			request.getRequestDispatcher("/welcome.jsp").forward(request, response);
+			//response.setHeader("Refresh","1;url=welcome.jsp");
+		}
+		
+	}
+
+}
